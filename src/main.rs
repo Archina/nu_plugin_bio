8391fd21bf2f3e63c1937bd324fbc3bde63be7bc
Archina <archina5thrider@gmail.com>
2025-05-14 13:25:04 +0200

    fix msgPack

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use nu_plugin::{serve_plugin, MsgPackSerializer};
 use nu_plugin_bio::Bio;
 
 fn main() {
-    serve_plugin(&mut Bio {}, MsgPackSerializerSerializer {})
+    serve_plugin(&mut Bio {}, MsgPackSerializer {})
 }
